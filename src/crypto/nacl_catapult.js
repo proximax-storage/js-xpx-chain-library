e157304edfbec7016799f0736aee18ea5d679efc
Yaaccount <33345431+yaaccount@users.noreply.github.com>
2019-04-28 00:46:02 +0200

    crypto_shared_key_hash as in upstream

--- src/crypto/nacl_catapult.js
+++ src/crypto/nacl_catapult.js
@@ -2106,7 +2106,7 @@
 		d[31] |= 64;
 
 		var q = [gf(), gf(), gf(), gf()];
-		unpack(q, pk);
+		unpackneg(q, pk);
 		scalarmult(p, q, d);
 		pack(shared, p);
 	}
@@ -2197,44 +2197,6 @@
 
 		modL(sm.subarray(32), x);
 		return smlen;
-	}
-
-	function unpack(r, p) {
-		var t = gf(), chk = gf(), num = gf(),
-			den = gf(), den2 = gf(), den4 = gf(),
-			den6 = gf();
-
-		set25519(r[2], gf1);
-		unpack25519(r[1], p);
-		S(num, r[1]);
-		M(den, num, D);
-		Z(num, num, r[2]);
-		A(den, r[2], den);
-
-		S(den2, den);
-		S(den4, den2);
-		M(den6, den4, den2);
-		M(t, den6, num);
-		M(t, t, den);
-
-		pow2523(t, t);
-		M(t, t, num);
-		M(t, t, den);
-		M(t, t, den);
-		M(r[0], t, den);
-
-		S(chk, r[0]);
-		M(chk, chk, den);
-		if (neq25519(chk, num)) M(r[0], r[0], I);
-
-		S(chk, r[0]);
-		M(chk, chk, den);
-		if (neq25519(chk, num)) return -1;
-
-		if (par25519(r[0]) !== (p[31] >> 7)) Z(r[0], gf0, r[0]);
-
-		M(r[3], r[0], r[1]);
-		return 0;
 	}
 
 	function unpackneg(r, p) {
@@ -2349,6 +2311,7 @@
 		crypto_sign_keypair: crypto_sign_keypair,
 		crypto_sign_open: crypto_sign_open,
 		crypto_shared_key_hash: crypto_shared_key_hash,
+
 		crypto_secretbox_KEYBYTES: crypto_secretbox_KEYBYTES,
 		crypto_secretbox_NONCEBYTES: crypto_secretbox_NONCEBYTES,
 		crypto_secretbox_ZEROBYTES: crypto_secretbox_ZEROBYTES,
