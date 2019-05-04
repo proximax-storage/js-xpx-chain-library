5f5d342bc1a539fa5f1c383e2dade76518b1e11f
Yaaccount <33345431+yaaccount@users.noreply.github.com>
2019-05-04 21:46:11 +0200

    pad ripemd160 20 bytes secret to 32 bytes in SecretLockTransaction

--- src/transactions/SecretLockTransaction.js
+++ src/transactions/SecretLockTransaction.js
@@ -98,7 +98,7 @@ export default class SecretLockTransaction extends VerifiableTransaction {
 				const mosaicIdVector = SecretLockTransactionBuffer.createMosaicIdVector(builder, this.mosaicId);
 				const mosaicAmountVector = SecretLockTransactionBuffer.createMosaicAmountVector(builder, this.mosaicAmount);
 				const durationVector = SecretLockTransactionBuffer.createDurationVector(builder, this.duration);
-				const byteSecret = convert.hexToUint8(this.secret);
+				const byteSecret = convert.hexToUint8((this.secret + '0'.repeat(64-this.secret.length))); // pad to 64 hex chars
 				const secretVector = SecretLockTransactionBuffer.createSecretVector(builder, byteSecret);
 				const recipientVector = SecretLockTransactionBuffer.createRecipientVector(builder, this.recipient);
 
